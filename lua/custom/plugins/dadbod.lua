95116fa18b2c26d03be5ddb51e5957a0ceac2bf0
Alexander Söderholm (gry346) <alexander.soderholm@teliacompany.com>
2025-01-14 15:28:54 +0100

    Eager load dadbod

--- lua/custom/plugins/dadbod.lua
+++ lua/custom/plugins/dadbod.lua
@@ -1,8 +1,8 @@
 return {
   'kristijanhusak/vim-dadbod-ui',
   dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    { 'tpope/vim-dadbod', lazy = false },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = false },
   },
   cmd = {
     'DBUI',
